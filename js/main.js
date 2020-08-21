80765ed495155ac747f89d99a5ac2ee28938fc92
ZeLars3 <68004200+ZeLars3@users.noreply.github.com>
2020-08-21 22:30:39 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+AOS.init();
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActievClass: 'projects-bullet-active',
+    },
+});
